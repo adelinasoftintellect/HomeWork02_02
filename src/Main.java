7d8bc6091160a82dfd34bacf645f861a7d6a71ed
Adi <adelina_yordanova11@abv.bg>
2016-05-21 13:13:09 +0300

    HomeWork02_02.

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 5;
+		System.out.println("a " + a);
+		int b = 8;
+		System.out.println("b " + b);
+
+		a = b;
+		System.out.println("a " + a);
+
+		a = 5;
+		b = a;
+		System.out.println("b " + b);
+
+	}
+
+}
